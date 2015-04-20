06b4d95a49324c7dc23bcc7849a38270d41f1229
Kafia <kafia@sutlej.net>
2015-04-20 15:25:39 +0500

    Widgets added

---
+++ includes/widgets/lock-screen.php
@@ -0,0 +1,25 @@
+   <!-- START LOCK SCREEN ITEM -->
+      <div class="lockscreen-item">
+        <!-- lockscreen image -->
+        <div class="lockscreen-image">
+          <img src="../../dist/img/user1-128x128.jpg" alt="user image"/>
+        </div>
+        <!-- /.lockscreen-image -->
+
+        <!-- lockscreen credentials (contains the form) -->
+        <form class="lockscreen-credentials">
+          <div class="input-group">
+            <input type="password" class="form-control" placeholder="password" />
+            <div class="input-group-btn">
+              <button class="btn"><i class="fa fa-arrow-right text-muted"></i></button>
+            </div>
+          </div>
+        </form><!-- /.lockscreen credentials -->
+
+      </div><!-- /.lockscreen-item -->
+      <div class="help-block text-center">
+        Enter your password to retrieve your session
+      </div>
+      <div class='text-center'>
+        <a href="login.html">Or sign in as a different user</a>
+      </div>  <!-- /.text-center-->
