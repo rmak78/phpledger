7d11896131610be366f32dad64acfd5a454d3846
Rana Mansoor Akbar Khan <mansoor@sutlej.net>
2015-04-20 11:25:49 +0500

    Removed all files
    Pheonex has to burn to ashes

--- ajax_helpers/ajax_check_account_code.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once ( '../tools_header.php');
-require_once ("../functions.php");
-if(isset($_POST['account_code'])){ // Check for the group_code_availability posted
-    $account_code = $_POST['account_code']; // Get the username values
-	$account_check = check_coa_account_code_exist($account_code);
-	echo $account_check;
-}
-?>
