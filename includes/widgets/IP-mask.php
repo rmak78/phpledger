5278bf1d2e92a4a26eb597280cb3eb127ca4b969
Kafia <kafia@sutlej.net>
2015-04-20 14:15:54 +0500

    added to widgets

---
+++ includes/widgets/IP-mask.php
@@ -0,0 +1,10 @@
+<!-- IP mask -->
+                  <div class="form-group">
+                    <label>IP mask:</label>
+                    <div class="input-group">
+                      <div class="input-group-addon">
+                        <i class="fa fa-laptop"></i>
+                      </div>
+                      <input type="text" class="form-control" data-inputmask="'alias': 'ip'" data-mask/>
+                    </div><!-- /.input group -->
+                  </div><!-- /.form group -->
---
+++ includes/widgets/date-mask-dd_mm_yy.php
@@ -0,0 +1,11 @@
+<div class="box-body">
+                  <!-- Date dd/mm/yyyy -->
+                  <div class="form-group">
+                    <label>Date masks:</label>
+                    <div class="input-group">
+                      <div class="input-group-addon">
+                        <i class="fa fa-calendar"></i>
+                      </div>
+                      <input type="text" class="form-control" data-inputmask="'alias': 'dd/mm/yyyy'" data-mask/>
+                    </div><!-- /.input group -->
+                  </div><!-- /.form group -->
