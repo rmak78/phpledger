fbd18f97b6dc92e9c2de90dff5b7348b5e8a3558
waseemshahzad238 <waseem-s786@hotmail.com>
2015-04-20 14:13:05 +0500

    session variables

--- includes/config.php
+++ includes/config.php
@@ -10,12 +10,25 @@ if(session_id() == '') {
 define('SYSTEM_ENCODING', 'utf8' );
 define('BR','</br>');
 
+include_once('db_config.php');
+	
 
  
-define('FOLDER_NAME','phpledger2');
-define('ROOT_PATH', 'D:\server\www'.'/'.FOLDER_NAME.'/');
+define('FOLDER_NAME','phpledger');
+define('ROOT_PATH', 'E:\xampp\htdocs'.'/'.FOLDER_NAME.'/');
 define('SITE_ROOT', 'http://'.$_SERVER['HTTP_HOST'].'/'.FOLDER_NAME.'/');
 define('DB_PREFIX', 'sa_');
+
+
+	$_SESSION['is_logged'] = 1; 
+	$_SESSION['company_id'] =1 ;
+	$_SESSION['user_id'] =1;
+	$_SESSION['user_name'] ='test';
+	$_SESSION['role_id'] = 1;
+	$_SESSION['co_prefix'] = "test_";
+	$_SESSION['default_expense_account'] = 1; // get default Expense Account Company
+	$_SESSION['DB_PREFIX'] = 'sa_';
+ 	$_SESSION['company_name'] = 'Sutlej Solutions';
  
-include_once('db_config.php');
- 
+
+	?>
