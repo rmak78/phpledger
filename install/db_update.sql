1b8f5723c70ab683db3b9d7cb0207216db0a9922
Rana Mansoor Akbar Khan <mansoor@sutlej.net>
2015-04-23 16:42:12 +0500

    Fixed more errors

--- install/db_update.sql
+++ install/db_update.sql
@@ -192,7 +192,7 @@ DROP TABLE IF EXISTS `sa_test_reporting_periods`;
 CREATE TABLE `sa_test_reporting_periods` (
   `reporting_period_id` int(11) NOT NULL AUTO_INCREMENT,
   `fiscal_year_id` int(11) DEFAULT NULL,
-  `reporting_period_description` varchar(255) DEFAULT NULL,
+  `reporting_period_desc` varchar(255) DEFAULT NULL,
   `reporting_period_start_date` date DEFAULT NULL,
   `reporting_period_end_date` date DEFAULT NULL,
   `last_modified_by` varchar(255) DEFAULT NULL,
@@ -205,7 +205,7 @@ CREATE TABLE `sa_test_reporting_periods` (
 
 /*Data for the table `sa_test_reporting_periods` */
 
-insert  into `sa_test_reporting_periods`(`reporting_period_id`,`fiscal_year_id`,`reporting_period_description`,`reporting_period_start_date`,`reporting_period_end_date`,`last_modified_by`,`last_modified_on`,`created_by`,`created_on`,`rp_status`) values (1,1,NULL,NULL,'2015-08-31',NULL,'2015-04-23 16:34:29',NULL,NULL,'active'),(2,1,'Q2-2014','2014-09-01','2014-10-31',NULL,'2015-04-23 16:36:24',NULL,NULL,'active'),(3,1,'Q3-2014','2014-11-01','2015-01-31',NULL,'2015-04-23 16:37:19',NULL,NULL,'active');
+insert  into `sa_test_reporting_periods`(`reporting_period_id`,`fiscal_year_id`,`reporting_period_desc`,`reporting_period_start_date`,`reporting_period_end_date`,`last_modified_by`,`last_modified_on`,`created_by`,`created_on`,`rp_status`) values (1,1,'Q1-2014','2014-06-01','2015-08-31',NULL,'2015-04-23 16:34:29',NULL,NULL,'active'),(2,1,'Q2-2014','2014-09-01','2014-10-31',NULL,'2015-04-23 16:36:24',NULL,NULL,'active'),(3,1,'Q3-2014','2014-11-01','2015-01-31',NULL,'2015-04-23 16:37:19',NULL,NULL,'active');
 
 /*Table structure for table `sa_test_users` */
 
