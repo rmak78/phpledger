0d901e9cc70c508492a8dbe41bf61b206f1dfbe1
Waseem Mehmood <waseem@sutlej.net>
2015-04-25 14:33:43 +0500

    change form style
    adding modals

---
+++ modules/gl/transactions/expense/add_expense_voucher_detail.php
@@ -0,0 +1,122 @@
+        <!-- Main content -->
+        <section class="invoice">
+          <!-- title row -->
+          <div class="box">
+            <div class="box-header with-border">
+              <h3 class="box-title">Add Expense Voucher Details</h3>
+              <div class="box-tools pull-right">
+                <button class="btn btn-box-tool" data-widget="collapse" data-toggle="tooltip" title="Collapse"><i class="fa fa-minus"></i></button>
+                <button class="btn btn-box-tool" data-widget="remove" data-toggle="tooltip" title="Remove"><i class="fa fa-times"></i></button>
+              </div>
+            </div>
+<div class="box-body">
+     <div class="progress">
+		<div class="progress-bar progress-bar-success progress-bar-striped" role="progressbar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100" style="width: 50%">
+        <span class="sr-only">100% Complete  </span>
+        </div>
+      </div>		  
+          <div class="row">
+            <div class="col-xs-12">
+              <h2 class="page-header">
+                <i class="fa fa-globe"></i> Sutlej Solutions.
+                <small class="pull-right"><?php echo date("j / n / Y"); ?></small>
+              </h2>
+            </div><!-- /.col -->
+          </div>
+          <!-- info row -->
+          <div class="row invoice-info">
+            <div class="col-sm-4 invoice-col">
+                <strong>Voucher Description</strong>
+				<BR/>
+            </div><!-- /.col -->
+            <div class="col-sm-4 invoice-col">
+              <b>Invoice #007612</b><br/>
+              <b>Voucher Ref#:</b> 4F3S8J<br/>
+              <b>Voucher Date:</b> 2/22/2014<br/>
+              <b>Paid from Account:</b> 968-34567
+            </div><!-- /.col -->
+          </div><!-- /.row -->
+		</div>
+	</div>	
+          <!-- Table row -->
+          <div class="row">
+            <div class="col-xs-12 table-responsive">
+              <table class="table table-striped">
+                <thead>
+                  <tr>
+                    <th>Qty</th>
+                    <th>Product</th>
+                    <th>Serial #</th>
+                    <th>Description</th>
+                    <th>Subtotal</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>1</td>
+                    <td>Call of Duty</td>
+                    <td>455-981-221</td>
+                    <td>El snort testosterone trophy driving gloves handsome</td>
+                    <td>$64.50</td>
+                  </tr>
+                  <tr>
+                    <td>1</td>
+                    <td>Need for Speed IV</td>
+                    <td>247-925-726</td>
+                    <td>Wes Anderson umami biodiesel</td>
+                    <td>$50.00</td>
+                  </tr>
+                  <tr>
+                    <td>1</td>
+                    <td>Monsters DVD</td>
+                    <td>735-845-642</td>
+                    <td>Terry Richardson helvetica tousled street art master</td>
+                    <td>$10.70</td>
+                  </tr>
+                  <tr>
+                    <td>1</td>
+                    <td>Grown Ups Blue Ray</td>
+                    <td>422-568-642</td>
+                    <td>Tousled lomo letterpress</td>
+                    <td>$25.99</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div><!-- /.col -->
+          </div><!-- /.row -->
+
+          <div class="row">
+            <div class="col-xs-6">
+              <p class="lead">Amount Due 2/22/2014</p>
+              <div class="table-responsive">
+                <table class="table">
+                  <tr>
+                    <th style="width:50%">Subtotal:</th>
+                    <td>$250.30</td>
+                  </tr>
+                  <tr>
+                    <th>Tax (9.3%)</th>
+                    <td>$10.34</td>
+                  </tr>
+                  <tr>
+                    <th>Shipping:</th>
+                    <td>$5.80</td>
+                  </tr>
+                  <tr>
+                    <th>Total:</th>
+                    <td>$265.24</td>
+                  </tr>
+                </table>
+              </div>
+            </div><!-- /.col -->
+          </div><!-- /.row -->
+
+          <!-- this row will not appear when printing -->
+          <div class="row no-print">
+            <div class="col-xs-12">
+              <a href="invoice-print.php" target="_blank" class="btn btn-default"><i class="fa fa-print"></i> Print</a>
+              <button class="btn btn-success pull-right"><i class="fa fa-credit-card"></i> Submit Payment</button>
+              <button class="btn btn-primary pull-right" style="margin-right: 5px;"><i class="fa fa-download"></i> Generate PDF</button>
+            </div>
+          </div>
+        </section>
