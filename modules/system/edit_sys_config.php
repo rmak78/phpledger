a0a4ebb70b55f3a6b9c1532d155d7b6112a8093f
Zahid Badar <mzahidbadar@hotmail.com>
2015-04-25 23:50:31 -0700

    edit page path correct insert new row
    edit page path correct insert new row

--- modules/system/edit_sys_config.php
+++ modules/system/edit_sys_config.php
@@ -1,5 +1,10 @@
-<?php
-
+<?php 
+$config_id= "";
+if(isset($_GET['config_id'])){
+	$config_id=$_GET['config_id'];
+}
+else { echo "not ";}
+?>
 <!-- Content Header (Page header) -->
   <section class="content-header">
            <h1>
@@ -10,3 +15,22 @@
                    <li class ="active"> Edit System Configrations </li>
                 </ol>
   </section>
+  <section class="content">
+
+ <!-- Default box -->
+          <div class="box">
+            <div class="box-header with-border">
+              <h3 class="box-title">System Configrations</h3>
+              <div class="box-tools pull-right">
+                <button class="btn btn-box-tool" data-widget="collapse" data-toggle="tooltip" title="Collapse"><i class="fa fa-minus"></i></button>
+                <button class="btn btn-box-tool" data-widget="remove" data-toggle="tooltip" title="Remove"><i class="fa fa-times"></i></button>
+              </div>
+            </div>
+            <div class="box-body">
+				<?php 	echo $config_id; ?>
+            </div><!-- /.box-body -->
+            <div class="box-footer">
+             <small> Please do not make changes to these unless you are really sure what you are doing. making changes here have system wide impact</small>
+            </div><!-- /.box-footer-->
+          </div><!-- /.box -->
+     	 </section><!-- /.content -->
