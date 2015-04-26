95733c0e409945257110dbd1ced9b37c0b84ed54
Zahid Badar <mzahidbadar@hotmail.com>
2015-04-25 23:06:48 -0700

    sys config and Edit
    show data in table made new file of edit

---
+++ modules/system/edit_sys_config.php
@@ -0,0 +1,12 @@
+<?php
+
+<!-- Content Header (Page header) -->
+  <section class="content-header">
+           <h1>
+                Edit System Configrations
+           </h1>
+                <ol class="breadcrumb">
+                   <li><a href ="<?php echo SITE_ROOT; ?>"><i class = "fa fa-dashboard"></i> Home</a></li>
+                   <li class ="active"> Edit System Configrations </li>
+                </ol>
+  </section>
