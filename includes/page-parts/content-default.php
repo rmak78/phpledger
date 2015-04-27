56720ac5440dae92f4dce3f88124e3c029eca64c
waseemshahzad238 <waseem-s786@hotmail.com>
2015-04-27 13:11:59 +0500

    use box in index page
    in content-default page  made boxes

--- includes/page-parts/content-default.php
+++ includes/page-parts/content-default.php
@@ -25,39 +25,72 @@
               </div>
             </div>
             <div class="box-body">
-              Week 17 of 2015 will be amazing!
-			  
-			  AdminLTE - Free Premium Admin control Panel Theme That Is Based On Bootstrap 3. .... When you contribute, you agree to give a non-exclusive license to Almsaeed ... Updated documentation and fixed links scrolling issue; Created print.less file ... issue in CSS, LESS, and JS; Updated box-shadow for boxes; Updated docs.
-admin-lte/CHANGES.md at master · dataloop ... - GitHub
-https://github.com/dataloop/admin-lte/blob/master/js/.../CHANGES.md
-admin-lte - AdminLTE - Free Premium Admin control Panel Theme That Is Based On Bootstrap 3.x. ... #11198: Widgets: Drag handler is not fully visible when an inline widget is in a heading. ..... #9945: [iOS] Scrolling not possible on iPad. .... #9528: Host page box-sizing style should not influence the editor UI elements.
-ShapeBootstrap | AdminLTE – Dashboard and Control Panel
-shapebootstrap.net › Items › Html › Admin Templates
-Apr 11, 2014 - AdminLTE 2 Dashboard ... AdminLTE — is a fully responsive admin template. ... Fixed the sidebar scroll issue when using the fixed layout.
-Admin LTE - Bootstrap Stage
-www.bootstrapstage.com/admin-lte/
-Built on top of Bootstrap 3, Admin LTE, is a high quality free dashboard theme. ... Sparkilne; Full Calendar; iCheck; jQuery input mask; jQuery Knob; jVector Map; Slim Scroll; Pace ... And what will I do to show all check box is checked or not. 2.
-[PDF]User Guide - Verizon Wireless
-cache.vzw.com/.../mim/...4glte-router/vzw-4glte-router-user-manual.pdf
-Sep 24, 2013 - without notice and should not be construed as a commitment by Novatel Wireless, Inc. Patents and ... U.S. Government Export Administration Act Compliance Clause: It is forbidden by US law ... Using the 4G LTE Broadband Router with Voice . ..... Scroll and select this icon to view the amount of data used in.
-Mobile HotSpot web portal (Admin page): Samsung... | T ...
-https://support.t-mobile.com/docs/DOC-9064
-T?Mobile US, Inc.
-Sep 25, 2013 - Mobile HotSpot web portal (Admin page): Samsung LTE Mobile HotSpot PRO ... Changing the administrator password does not change the password you use to connect other devices to your ... Scroll down to the Wi-Fi Configuration section. ... Click the box to check or un-check Enable MAC Filtering.
-ProUI - Responsive Bootstrap Admin Template - ThemeForest
-themeforest.net/item/proui-responsive-bootstrap-admin.../6769035
-Rating: 5 - ý229 votes - ý$21.00
-Feb 5, 2014 - ProUI is a responsive and full featured Admin and Frontend Template ... Even if you are not familiar with the framework, template's UI components can .... Main Sidebar Menu Auto Scrolling (when a submenu opens/closes).
-Angulr - Bootstrap Admin Web App with AngularJS
-themeforest.net/item/angulr-bootstrap-admin-web-app-with.../8437259
-Rating: 4.8 - ý246 votes - ý$21.00
-Aug 3, 2014 - Admin web application template with Bootstrap 3 and AngularJS. ... Added ui-scroll; Added smart table; Added date-range-picker; Added ... 1.3.2; Update angular-bootstrap to 0.12.0; Update angular-ui-router to 0.2.12 ... Use, by you or one client, in a single end product which end users are not charged for.
-Free Bootstrap Admin Templates for Designers - Code Geekz
-codegeekz.com/free-bootstrap-admin-templates-designers/
-Sep 25, 2014 - AdminLTE – is a fully responsive admin template. ... Bootstrap theme with demo and additional UI components not included in the bootstrap packaging. Fixed footer, admin panels, shadow box and more. ... template features a collapsible vertical sidebar, font-awesome icons, alerts and scrolling tables.
-[PDF]Verizon Jetpack MiFi 5510L User Guide - Novatel Wireless
-www.nvtl.com/index.php/download_file/view/538/
-Jan 30, 2013 - without notice and should not be construed as a commitment by Novatel Wireless, Inc. ... Export Administration Act, or any successor legislation, or in violation of the ..... Congratulations on your purchase of the Verizon Jetpack™ 4G LTE Mobile ... Scroll and select the icon to return to the Jetpack Home page.
+				<div class="row">
+					<div class="col-sm-6 col-md-6">
+						<div class="box box-default">
+							<div class="box-header with-border">
+							<h3 class="box-title">Browser Usage</h3>
+								<div class="box-tools pull-right">
+									<button class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i></button>
+									<button class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i></button>
+								</div>
+							</div> <!--/.Box Header -->
+							<div class="box-body">
+							<p> ...</p>
+							</div>
+							 <div class="box-footer no-padding">
+							 </div> <!--/.Box footer -->
+						</div>   <!--/.Box  -->
+					</div>
+					<div class="col-sm-6 col-md-6">
+						<div class="box box-default">
+							<div class="box-header with-border">
+							<h3 class="box-title">Browser Usage</h3>
+								<div class="box-tools pull-right">
+									<button class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i></button>
+									<button class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i></button>
+								</div>
+							</div> <!--/.Box Header -->
+							<div class="box-body">
+							<p> ...</p>
+							</div>
+							 <div class="box-footer no-padding">
+							 </div> <!--/.Box footer -->
+						</div>   <!--/.Box  -->
+					</div>
+				</div>  <!--/.row  -->
+				<div class="row"> <!-- Row Containing Thumbnail-->
+					  <div class="col-sm-6 col-md-4"> <!-- 1st Thumbnail -->
+						<div class="thumbnail">
+						 <img data-holder-rendered="true" src="data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iVVRGLTgiIHN0YW5kYWxvbmU9InllcyI/PjxzdmcgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB3aWR0aD0iMjQyIiBoZWlnaHQ9IjIwMCIgdmlld0JveD0iMCAwIDI0MiAyMDAiIHByZXNlcnZlQXNwZWN0UmF0aW89Im5vbmUiPjxkZWZzLz48cmVjdCB3aWR0aD0iMjQyIiBoZWlnaHQ9IjIwMCIgZmlsbD0iI0VFRUVFRSIvPjxnPjx0ZXh0IHg9IjkxLjUiIHk9IjEwMCIgc3R5bGU9ImZpbGw6I0FBQUFBQTtmb250LXdlaWdodDpib2xkO2ZvbnQtZmFtaWx5OkFyaWFsLCBIZWx2ZXRpY2EsIE9wZW4gU2Fucywgc2Fucy1zZXJpZiwgbW9ub3NwYWNlO2ZvbnQtc2l6ZToxMXB0O2RvbWluYW50LWJhc2VsaW5lOmNlbnRyYWwiPjI0MngyMDA8L3RleHQ+PC9nPjwvc3ZnPg==" style="height: 200px; width: 100%; display: block;" data-src="holder.js/100%x200" alt="100%x200">
+						  <div class="caption">
+							<h3>Thumbnail label</h3>
+							<p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+							<p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+						  </div>
+						</div>
+					  </div>
+					  <div class="col-sm-6 col-md-4"> <!-- 2nd Thumbnail -->
+						<div class="thumbnail">
+						 <img data-holder-rendered="true" src="data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iVVRGLTgiIHN0YW5kYWxvbmU9InllcyI/PjxzdmcgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB3aWR0aD0iMjQyIiBoZWlnaHQ9IjIwMCIgdmlld0JveD0iMCAwIDI0MiAyMDAiIHByZXNlcnZlQXNwZWN0UmF0aW89Im5vbmUiPjxkZWZzLz48cmVjdCB3aWR0aD0iMjQyIiBoZWlnaHQ9IjIwMCIgZmlsbD0iI0VFRUVFRSIvPjxnPjx0ZXh0IHg9IjkxLjUiIHk9IjEwMCIgc3R5bGU9ImZpbGw6I0FBQUFBQTtmb250LXdlaWdodDpib2xkO2ZvbnQtZmFtaWx5OkFyaWFsLCBIZWx2ZXRpY2EsIE9wZW4gU2Fucywgc2Fucy1zZXJpZiwgbW9ub3NwYWNlO2ZvbnQtc2l6ZToxMXB0O2RvbWluYW50LWJhc2VsaW5lOmNlbnRyYWwiPjI0MngyMDA8L3RleHQ+PC9nPjwvc3ZnPg==" style="height: 200px; width: 100%; display: block;" data-src="holder.js/100%x200" alt="100%x200">
+						  <div class="caption">
+							<h3>Thumbnail label</h3>
+							<p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+							<p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+						  </div>
+						</div>
+					  </div>
+					  <div class="col-sm-6 col-md-4"> <!-- #3rd Thumbnail -->
+						<div class="thumbnail">
+						 <img data-holder-rendered="true" src="data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iVVRGLTgiIHN0YW5kYWxvbmU9InllcyI/PjxzdmcgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB3aWR0aD0iMjQyIiBoZWlnaHQ9IjIwMCIgdmlld0JveD0iMCAwIDI0MiAyMDAiIHByZXNlcnZlQXNwZWN0UmF0aW89Im5vbmUiPjxkZWZzLz48cmVjdCB3aWR0aD0iMjQyIiBoZWlnaHQ9IjIwMCIgZmlsbD0iI0VFRUVFRSIvPjxnPjx0ZXh0IHg9IjkxLjUiIHk9IjEwMCIgc3R5bGU9ImZpbGw6I0FBQUFBQTtmb250LXdlaWdodDpib2xkO2ZvbnQtZmFtaWx5OkFyaWFsLCBIZWx2ZXRpY2EsIE9wZW4gU2Fucywgc2Fucy1zZXJpZiwgbW9ub3NwYWNlO2ZvbnQtc2l6ZToxMXB0O2RvbWluYW50LWJhc2VsaW5lOmNlbnRyYWwiPjI0MngyMDA8L3RleHQ+PC9nPjwvc3ZnPg==" style="height: 200px; width: 100%; display: block;" data-src="holder.js/100%x200" alt="100%x200">
+						  <div class="caption">
+							<h3>Thumbnail label</h3>
+							<p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+							<p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+						  </div>
+						</div>
+					</div>
+                </div>
             </div><!-- /.box-body -->
             <div class="box-footer">
               Footer
