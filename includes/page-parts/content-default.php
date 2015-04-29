3fbc6892d748a7e2f2fed6b404076e73611ec379
Rana Mansoor Akbar Khan <mansoor@sutlej.net>
2015-04-29 09:24:13 +0500

    some changes

--- includes/page-parts/content-default.php
+++ includes/page-parts/content-default.php
@@ -2,7 +2,7 @@
         <!-- Content Header (Page header) -->
         <section class="content-header">
           <h1>
-           PHP Ledger Dashboard ( Test Branch)
+           PHP Ledger Dashboard ( Master Branch & Test Merged)
             <small>it all starts here</small>
           </h1>
           <ol class="breadcrumb">
